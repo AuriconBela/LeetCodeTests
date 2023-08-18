e8e3015632982b722370da8e025349a71aa2f9c4
Balazs, Bela (ADV D EU HU OPS 2 MPS 8) <bela.balazs@evosoft.com>
2023-08-18 15:25:25 +0200

    First working attempt

--- TrappingRainWater.cs
+++ TrappingRainWater.cs
@@ -2,31 +2,54 @@
 
 public class TrappingRainWater
 {
-    private List<Range> _pools = new List<Range>();
+    private readonly List<int[]> _pools = new();
+
     public int Trap(int[] height)
     {
         InnerIteration(height);
-        return _pools.Count;
+        return ResultCalc();
     }
 
     private void InnerIteration(int[] array)
     {
         var heighestIndex = -1;
 
-        var leftMost = array;
-        while (TryFindHighest(leftMost, ref heighestIndex))
+        //if (IsPlateu(array)) return;
+
+        while (TryFindHighest(array, ref heighestIndex))
         {
-            var rightMost = SplitAt(ref leftMost, heighestIndex);
+            var rightIndex = heighestIndex + 1;
+            var foundPool = false;
+            while (rightIndex < array.Length)
+            {
+                if (IsPool(array, heighestIndex, rightIndex))
+                {
+                    foundPool = true;
+                    break;
+                }
+                rightIndex++;
+            }
 
-            SearchForPools(leftMost);
-            SearchForPools(rightMost);
+            if (foundPool)
+            {
+                SplitAt(ref array, heighestIndex, rightIndex, out var middle, out var rightMost);
+                _pools.Add(middle);
 
-            InnerIteration(leftMost);
-            InnerIteration(rightMost);
+                InnerIteration(array);
+                InnerIteration(rightMost);
+                break;
+
+            }
+            else
+            {
+                if (array[heighestIndex] == 0) return;
+                array[heighestIndex]--;
+            }
         }
+
     }
 
-    private bool TryFindHighest(int[] array, ref int highestIndex)
+    private static bool TryFindHighest(int[] array, ref int highestIndex)
     {
         if (array.Length < 3) { return false; }
 
@@ -43,16 +66,60 @@ public class TrappingRainWater
         return true;
     }
 
-    private int[] SplitAt(ref int[] original, int pivot)
+    private static void SplitAt(ref int[] original, int splitStart, int splitEnd, out int[] middleArray, out int[] rightArray)
     {
-        var p = pivot + 1;
-        var tmp = original[p..];
-        original = original[..pivot];
-        return tmp;
+        var s = splitStart + 1;
+        var e = splitEnd + 1;
+
+        middleArray = original[splitStart..e];
+        rightArray = original[splitEnd..];
+        original = original[..s];
     }
 
-    private void SearchForPools(int[] array)
+    private static bool IsPool(int[] array, int startIndex, int endIndex)
     {
+        if (endIndex - startIndex < 2) return false;
 
+        if (array[startIndex] != array[endIndex]) return false;
+
+        for (var i = startIndex + 1; i < endIndex; i++)
+        {
+            if (array[i] >= array[startIndex]) return false;
+        }
+
+        return true;
+    }
+
+    //private static bool IsPlateu(int[] array)
+    //{
+    //    if (array.Length < 2) return true;
+
+    //    var first = array[0];
+    //    for (int i = 1; i < array.Length; i++)
+    //    {
+    //        if (array[i] != first) return false;
+    //    }
+    //    return true;
+    //}
+
+    private static int PoolDepth(int[] array)
+    {
+        var result = 0;
+        var maxi = array[0];
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            result += maxi - array[i];
+        }
+        return result;
+    }
+
+    private int ResultCalc()
+    {
+        var result = 0;
+        foreach (var i in _pools)
+        {
+            result += PoolDepth(i);
+        }
+        return result;
     }
 }
